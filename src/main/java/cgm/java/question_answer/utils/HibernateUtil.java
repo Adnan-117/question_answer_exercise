bababcc1fbf2f9bd2336f2f24542ba0283e495d6
adn <adn@celum.com>
2020-04-06 12:46:17 +0200

    New tests

--- src/main/java/cgm/java/question_answer/utils/HibernateUtil.java
+++ src/main/java/cgm/java/question_answer/utils/HibernateUtil.java
@@ -56,7 +56,7 @@ public class HibernateUtil {
     //    settings.put(Environment.SHOW_SQL, "true");
     settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
     //        settings.put(Environment.HBM2DDL_AUTO, "update");
-    settings.put(Environment.HBM2DDL_AUTO, "update");
+    settings.put(Environment.HBM2DDL_AUTO, "create-drop");
     return settings;
   }
 
